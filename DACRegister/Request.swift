2e9063447ef96331d5e043d3605e25d4ea027cc7
cwjoshuak <jkuan1997@gmail.com>
2018-05-12 13:21:08 -0700

    modified gitignore

---
+++ DACRegister/Request.swift
@@ -0,0 +1,11 @@
+//
+//  API.swift
+//  DACRegister
+//
+//  Created by Joshua Kuan on 11/05/2018.
+//  Copyright © 2018 Joshua Kuan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
