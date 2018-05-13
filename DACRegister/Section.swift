17015d53331091822b4eb39234b31f2362f48db7
cwjoshuak <jkuan1997@gmail.com>
2018-05-12 19:42:10 -0700

    section contains num and courses

---
+++ DACRegister/Section.swift
@@ -0,0 +1,20 @@
+//
+//  Section.swift
+//  DACRegister
+//
+//  Created by Joshua Kuan on 12/05/2018.
+//  Copyright © 2018 Joshua Kuan. All rights reserved.
+//
+
+import Foundation
+
+public struct Section {
+    var courses = [Course]()
+    var sectionNum : String
+    mutating func addCourse(_ course: Course) {
+        self.courses.append(course)
+    }
+    init(_ secNum: String) {
+        sectionNum = secNum
+    }
+}
