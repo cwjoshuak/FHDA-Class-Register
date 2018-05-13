7f349f723bdab788cb23f77b1760badd9ed28759
cwjoshuak <jkuan1997@gmail.com>
2018-05-12 19:43:11 -0700

    Request works for courseList and getSingle
    - courseList returns a list of courses
    - getSingle takes 1/2 params and returns dept or courses accordingly (not 100% implemented).

--- DACRegister/Request.swift
+++ DACRegister/Request.swift
@@ -8,4 +8,84 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
+class RequestController {
+    static let shared = RequestController()
+    
+    private init() { }
+    
+    // MARK: Functions
+    
+    /**
+     Actual HTTP Request
+     
+     - parameter url: URL to request
+     - parameter params: Optional URL Query Parameters to send
+     - parameter completion: Returns JSON data
+     */
+    
+    func request(
+        _ endpoint : Endpoint,
+        params: [String : String]? = nil,
+        then completion: @escaping ([String]?, Dept?, [Course]?) -> ()
+    ) {
+        let endpointInfo = endpoint.httpInfo
+        var urlString = "https://floof.li\(endpointInfo.url)"
+        var numParams = 0
+        if let params = params {
+            urlString += "?"
+            urlString += params.map(
+                { key, value in "\(key)=\(value)" }
+                ).joined(separator: "&")
+            numParams = params.count
+        }
+        
+        Alamofire.request(urlString).validate().responseJSON { (response) in
+            if let error = response.result.error {
+                print(error)
+                return
+            }
+            
+            switch(endpoint) {
+            case Endpoint.courseList:
+                let courseList = response.result.value as! String
+                let array = courseList.components(separatedBy: ", ")
+                completion(array, nil, nil)
+                return
+            case Endpoint.getSingle:
+                let json = JSON(response.result.value!)
+
+                if(numParams == 1) {
+                    var dept = Dept(params!["dept"]!)
+                    
+                    for (index, sections) in json {
+                        var count = 0
+                        for (section, courses):(String, JSON) in sections {
+                            let sec = Section(section)
+                            dept.addSection(sec)
+                            for(course, courseData):(String, JSON) in courses {
+                                for(numClass, cData):(String, JSON) in courseData {
+                                    dept.sections[count].addCourse(Course(cData))
+                                    print(dept.sections[count].courses)
+                                }
+                            }
+                            count += 1
+                        }
+                    }
+                    completion(nil, dept, nil)
+                    return
+                }
+            case Endpoint.getURLs:
+                completion(nil, nil, nil)
+            case Endpoint.postBatch:
+                completion(nil, nil, nil)
+                
+            }
+            
+        }
+        
+
+        print(urlString)
+    }
+}
